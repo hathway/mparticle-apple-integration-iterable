b7a2468576db54677f1337d34a5593c42616382f
Yevhenii Hutorov <yevhenii.hutorov@bounteous.com>
2023-09-08 13:30:36 +0300

    Add SPM Support

---
+++ Package.swift
@@ -0,0 +1,33 @@
+// swift-tools-version:5.3
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "mParticle-Iterable",
+    platforms: [ .iOS(.v11) ],
+    products: [
+        .library(
+            name: "mParticle-Iterable",
+            targets: ["mParticle-Iterable"]),
+    ],
+    dependencies: [
+      .package(name: "mParticle-Apple-SDK",
+               url: "https://github.com/mParticle/mparticle-apple-sdk",
+               .upToNextMajor(from: "8.0.0")),
+      .package(name: "IterableSDK",
+               url: "https://github.com/Iterable/swift-sdk.git",
+               .upToNextMajor(from: "6.2.0")),
+    ],
+    targets: [
+        .target(
+            name: "mParticle-Iterable",
+            dependencies: [
+              .byName(name: "mParticle-Apple-SDK"),
+              .product(name: "IterableSDK", package: "IterableSDK"),
+            ],
+            path: "mParticle-iterable",
+            exclude: ["Info.plist"],
+            publicHeadersPath: "."),
+    ]
+)
